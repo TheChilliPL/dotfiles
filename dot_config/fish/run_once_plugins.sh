393533aa6f7648741c36d81b9562e8d2fddec972
Chilli <chilli@thechilli.dev>
2025-10-27 12:17:43 +0100

    Add tide config

--- dot_config/fish/run_once_plugins.sh
+++ dot_config/fish/run_once_plugins.sh
@@ -6,3 +6,5 @@ if not type -q fisher
 end
 
 fisher install IlanCosman/tide@v6
+
+tide configure --auto --style=Rainbow --prompt_colors='True color' --show_time='24-hour format' --rainbow_prompt_separators=Vertical --powerline_prompt_heads=Sharp --powerline_prompt_tails=Flat --powerline_prompt_style='Two lines, character' --prompt_connection=Solid --powerline_right_prompt_frame=No --prompt_connection_andor_frame_color=Lightest --prompt_spacing=Sparse --icons='Few icons' --transient=No
